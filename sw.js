7c8ef417282e8a8b417efe5b3acd8d530a8c3857
bloomin1234 <152080284+bloomin1234@users.noreply.github.com>
2026-06-15 23:18:56 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'smart-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Archivo:wght@600;700;800;900&family=Noto+Sans+JP:wght@400;500;700;900&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
